ec344ad06b0c255484b3393d61dbe414a288a641
NettoJM <nettojm86@gmail.com>
2023-03-14 15:54:10 -0300

    create get endpoint "/talker" to get all users from "talker.json" file

--- src/index.js
+++ src/index.js
@@ -3,12 +3,19 @@ const express = require('express');
 const app = express();
 app.use(express.json());
 
+const talkerServices = require('./talkerServices');
+
 const HTTP_OK_STATUS = 200;
 const PORT = process.env.PORT || '3001';
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
+});
+
+app.get('/talker', async (_request, response) => {
+  const talkerData = await talkerServices.getAllUsers();
+  response.status(HTTP_OK_STATUS).send(talkerData);
 });
 
 app.listen(PORT, () => {
