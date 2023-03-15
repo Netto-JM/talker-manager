611b3878bf2554bd945012a60e8dd3d4334d0117
NettoJM <nettojm86@gmail.com>
2023-03-15 01:48:28 -0300

    add "validateTalk" validation with many helpers, refactored code for better organization and readability

--- src/validations.js
+++ src/validations.js
@@ -1,18 +1,20 @@
-// isValidDate = /^([0-2][0-9]|(3)[0-1])(\/)(((0)[0-9])|((1)[0-2]))(\/)\d{4}$/i;
+const checkEmailValidation = (email) => {
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return emailRegex.test(email);
+};
 
 const validateEmail = (request, response, next) => {
   const hasEmail = Object.prototype.hasOwnProperty.call(request.body, 'email');
   if (!hasEmail) {
     return response.status(400).send({
-      message: 'O campo \'email\' é obrigatório',
+      message: 'O campo "email" é obrigatório',
     });
   }
 
-  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-  const isValidEmail = emailRegex.test(request.body.email);
+  const isValidEmail = checkEmailValidation(request.body.email);
   if (!isValidEmail) {
     return response.status(400).send({
-      message: 'O \'email\' deve ter o formato \'email@email.com\'',
+      message: 'O "email" deve ter o formato "email@email.com"',
     });
   }
   next();
@@ -22,7 +24,7 @@ const validatePassword = (request, response, next) => {
   const hasPassword = Object.prototype.hasOwnProperty.call(request.body, 'password');
   if (!hasPassword) {
     return response.status(400).send({
-      message: 'O campo \'password\' é obrigatório',
+      message: 'O campo "password" é obrigatório',
     });
   }
 
@@ -30,7 +32,7 @@ const validatePassword = (request, response, next) => {
   const isValidPassword = request.body.password.length >= VALID_PASSWORD_LENGTH;
   if (!isValidPassword) {
     return response.status(400).send({
-      message: 'O \'password\' deve ter pelo menos 6 caracteres',
+      message: 'O "password" deve ter pelo menos 6 caracteres',
     });
   }
   next();
@@ -61,38 +63,117 @@ const validateName = (request, response, next) => {
   const hasName = Object.prototype.hasOwnProperty.call(request.body, 'name');
   if (!hasName) {
     return response.status(400).send({
-      message: 'O campo \'name\' é obrigatório',
+      message: 'O campo "name" é obrigatório',
     });
   }
 
-  const VALID_NAME_LENGTH = 16;
-  const isValidName = request.body.name.length >= VALID_NAME_LENGTH;
+  const MIN_NAME_LENGTH = 3;
+  const isValidName = request.body.name.length >= MIN_NAME_LENGTH;
   if (!isValidName) {
     return response.status(400).send({
-      message: 'O \'name\' deve ter pelo menos 3 caracteres',
+      message: 'O "name" deve ter pelo menos 3 caracteres',
     });
   }
   next();
+};
+
+const checkAgeValidation = (age) => {
+  const VALID_MIN_AGE = 18;
+  const isValidTypeAge = Number.isInteger(age);
+  const isOldEnoughAge = age >= VALID_MIN_AGE;
+  return isValidTypeAge && isOldEnoughAge;
 };
 
 const validateAge = (request, response, next) => {
   const hasAge = Object.prototype.hasOwnProperty.call(request.body, 'age');
   if (!hasAge) {
     return response.status(400).send({
-      message: 'O campo \'age\' é obrigatório',
+      message: 'O campo "age" é obrigatório',
     });
   }
 
-  const { age } = request.body;
-  const VALID_MIN_AGE = 16;
-  const isValidTypeAge = Number.isInteger(age);
-  const isOldEnoughAge = age >= VALID_MIN_AGE;
-  const isValidAge = isValidTypeAge && isOldEnoughAge;
+  const isValidAge = checkAgeValidation(request.body.age);
   if (!isValidAge) {
     return response.status(400).send({
       message: 'O campo "age" deve ser um número inteiro igual ou maior que 18',
     });
   }
+  next();
+};
+
+const checkDateValidation = (date) => {
+  const dateRegex = /^([0-2][0-9]|(3)[0-1])(\/)(((0)[0-9])|((1)[0-2]))(\/)\d{4}$/i;
+  return dateRegex.test(date);
+};
+
+const validateWatchedAt = (talk) => {
+  const hasWatchedAt = Object.prototype.hasOwnProperty.call(talk, 'watchedAt');
+  if (!hasWatchedAt) {
+    return {
+      isValideWatchedAt: false,
+      payload: { message: 'O campo "watchedAt" é obrigatório' },
+    };
+  }
+
+  const isValidDate = checkDateValidation(talk.watchedAt);
+  if (!isValidDate) {
+    return {
+      isValideWatchedAt: false,
+      payload: {
+        message: 'O campo "watchedAt" deve ter o formato "dd/mm/aaaa"',
+      },
+    };
+  }
+  return { isValideWatchedAt: true };
+};
+
+const checkRateValidation = (rate) => {
+  const VALID_MIN_RATE = 1;
+  const VALID_MAX_RATE = 5;
+  const isValidTypeRate = Number.isInteger(rate);
+  const isValidRangeRate = rate >= VALID_MIN_RATE && rate <= VALID_MAX_RATE;
+  return isValidTypeRate && isValidRangeRate;
+};
+
+const validateRate = (talk) => {
+  const hasRate = Object.prototype.hasOwnProperty.call(talk, 'rate');
+  if (!hasRate) {
+    return {
+      isValideRate: false,
+      payload: { message: 'O campo "rate" é obrigatório' },
+    };
+  }
+
+  const isValidRate = checkRateValidation(talk.rate);
+  if (!isValidRate) {
+      return {
+      isValideRate: false,
+      payload: {
+        message: 'O campo "rate" deve ser um número inteiro entre 1 e 5',
+      },
+    };
+  }
+  return { isValideRate: true };
+};
+
+const validateTalk = (request, response, next) => {
+  const hasTalk = Object.prototype.hasOwnProperty.call(request.body, 'talk');
+  if (!hasTalk) {
+    return response.status(400).send({
+      message: 'O campo "talk" é obrigatório',
+    });
+  }
+
+  const watchedAtValidation = validateWatchedAt(request.body.talk);
+  if (!watchedAtValidation.isValideWatchedAt) {
+    return response.status(400).send(watchedAtValidation.payload);
+  }
+
+  const rateValidation = validateRate(request.body.talk);
+  if (!rateValidation.isValideRate) {
+    return response.status(400).send(rateValidation.payload);
+  }
+
   next();
 };
 
@@ -102,4 +183,5 @@ module.exports = {
   validateToken,
   validateName,
   validateAge,
+  validateTalk,
 };
